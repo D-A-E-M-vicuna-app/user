afd89d99474829cc53555eb7a5c1b2f9c634be0c
BRAYAN ALEXIS MALDONADO CARRASCO <brayan.maldonado@alumnos.ucn.cl>
2024-03-18 15:50:55 -0300

    feat: process.env.PORT || 3000

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
       allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
   
   })
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log("[*] Awaiting RPC requests user");
 
 }
