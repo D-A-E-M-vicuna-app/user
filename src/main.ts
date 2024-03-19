ac0e71db401409e9fd02a0b18c1af5631936d5e0
BRAYAN ALEXIS MALDONADO CARRASCO <brayan.maldonado@alumnos.ucn.cl>
2024-03-19 11:01:33 -0300

    feat: change to NEXT_PUBLIC_GATEWAY_DOMAIN

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3002',
+    origin: `https://${process.env.NEXT_PUBLIC_GATEWAY_DOMAIN}` || 'http://localhost:3002',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       credentials: true,
       allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
