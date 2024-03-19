aa2c773ba32026dbedaa46c9972e380abf38900d
BRAYAN ALEXIS MALDONADO CARRASCO <brayan.maldonado@alumnos.ucn.cl>
2024-03-19 17:20:21 -0300

    feat: listo para usar en modo dev y produccion

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: `https://${process.env.NEXT_PUBLIC_GATEWAY_DOMAIN}` || 'http://localhost:3002',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-      allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
-  
-  })
+    origin: process.env.NEXT_PUBLIC_GATEWAY_DOMAIN ? `https://${process.env.NEXT_PUBLIC_GATEWAY_DOMAIN}` : 'http://localhost:3002',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+  });
   await app.listen(process.env.PORT || 3000);
   console.log("[*] Awaiting RPC requests user");
 
